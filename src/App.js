aff5fdf06107246f03a50231b36bc07b55869168
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2022-02-04 18:06:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ButtonContainer from './components/ButtonContainer';
+import ScreenContainer from './components/ScreenContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <ScreenContainer />
+        <ButtonContainer />
+      </div>
     </div>
   );
 }
---
+++ src/components/ScreenContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './ScreenContainer.css'
+export default function ScreenContainer() {
+  return (
+      <div className="screen-container"></div>
+  );
+}
